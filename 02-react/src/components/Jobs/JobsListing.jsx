61f7bcd9e6320c33da1f2fa023528bedade73975
Lukestas <julianenriqueluquelopez@gmail.com>
2025-10-26 11:30:14 -0600

    rendering JobCard components

--- 02-react/src/components/Jobs/JobsListing.jsx
+++ 02-react/src/components/Jobs/JobsListing.jsx
@@ -1,11 +1,12 @@
 import { JobCard } from "./JobCard"
 
 export function JobsListing({jobs}) {
+
+    const jobslist=jobs.map(job=><JobCard key={job.id} job={job}/>)
+
     return (
         <div className="jobs-listings">
-            {jobs.map((job, index) => (
-                <JobCard job={job} key={index}/>
-            ))}
+            {jobslist}
         </div>
     )
 }
